271c509790156274a2ba270fe27cc5fca9e021c7
jonathanbourriauddev <jonathan.bourriauddev@gmail.com>
2026-03-11 22:09:39 +0100

    Ajout de Kitty, Fish et Starship
    - Kitty: thème Catppuccin, transparence 80%, ligatures
    - Fish: shell avec alias et config
    - Starship: prompt Catppuccin Mocha riche

---
+++ fish/config.fish
@@ -0,0 +1,76 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+starship init fish | source
+# Bienvenue
+function fish_greeting
+    echo "🐟 Welcome to Fish + Starship!"
+    echo "📁 Config: ~/.config"
+end
+# Starship
+starship init fish | source
+
+# Aliases utiles
+alias ls "ls --color=auto"
+alias ll "ls -la"
+alias la "ls -A"
+alias l "ls -CF"
+
+alias gs "git status"
+alias ga "git add"
+alias gc "git commit"
+alias gp "git push"
+alias gl "git log --oneline --graph"
+
+alias hypr "cd ~/.config/hypr"
+alias way "cd ~/.config/waybar"
+alias kit "cd ~/.config/kitty"
+alias rof "cd ~/.config/rofi"
+
+# Variables d'environnement
+set -gx EDITOR nano
+set -gx VISUAL nano
+
+# Bienvenue
+function fish_greeting
+    echo "🐟 Welcome to Fish + Starship!"
+    echo "📁 Config: ~/.config"
+end
+
+# Alias pour recharger fish
+alias reload="exec fish"
+
+# Navigation rapide
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+# Raccourcis config
+alias conf="cd ~/.config"
+alias hypr="cd ~/.config/hypr"
+alias way="cd ~/.config/waybar"
+alias rofi="cd ~/.config/rofi"
+alias kitty="cd ~/.config/kitty"
+alias fishc="cd ~/.config/fish"
+alias starship="cd ~/.config/starship"
+
+# Mise à jour système
+alias update="sudo pacman -Syu"
+alias cleanup="sudo pacman -Sc"
+
+# Git
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git log --oneline --graph"
+
+# LS amélioré
+alias ls="ls --color=auto"
+alias ll="ls -la"
+alias la="ls -A"
+alias l="ls -CF"
+
+# Hyprland
+alias hreload="hyprctl reload"
+alias hkill="pkill Hyprland"
